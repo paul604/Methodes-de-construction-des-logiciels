6a7f98b78e6158594f545de034dff151a0cf6ab0
paul <paul604@users.noreply.github.com>
2016-11-16 11:21:48 +0100

    Update Matrice.java

--- TD1/src/matrice/Matrice.java
+++ TD1/src/matrice/Matrice.java
@@ -10,12 +10,12 @@ import java.util.HashMap;
  */
 public enum Matrice {
 
-    M1("exo1",new HashMap<String, HashMap<Etat, Etat>>()){
+    M1("exo1",Etat.E0,new HashMap<String, HashMap<Etat, Etat>>()){
 
         @Override
         public void creeat() {
         	
-            //C1
+            //C1 C2
         	HashMap<Etat, Etat> c1 = new HashMap<>();
         	c1.put(Etat.E0, Etat.H1);
         	c1.put(Etat.H1, Etat.H);
@@ -24,16 +24,7 @@ public enum Matrice {
         	c1.put(Etat.M1, Etat.M2);
         	c1.put(Etat.M2, Etat.FIN);
         	this.mat.put("0",c1);
-        	
-        	//C2
-        	HashMap<Etat, Etat> c2 = new HashMap<>();
-        	c2.put(Etat.E0, Etat.H1);
-        	c2.put(Etat.H1, Etat.H);
-        	c2.put(Etat.H2, Etat.H);
-        	c2.put(Etat.H, Etat.ERREUR);
-        	c2.put(Etat.M1, Etat.M2);
-        	c2.put(Etat.M2, Etat.FIN);
-        	this.mat.put("1",c2);
+        	this.mat.put("1",c1);
         	
         	//c3
         	HashMap<Etat, Etat> c3 = new HashMap<>();
@@ -55,7 +46,7 @@ public enum Matrice {
         	c4.put(Etat.M2, Etat.FIN);
         	this.mat.put("3",c4);
         	
-        	//c5
+        	//c5 c6
         	HashMap<Etat, Etat> c5 = new HashMap<>();
         	c5.put(Etat.E0, Etat.ERREUR);
         	c5.put(Etat.H1, Etat.H);
@@ -64,18 +55,9 @@ public enum Matrice {
         	c5.put(Etat.M1, Etat.M2);
         	c5.put(Etat.M2, Etat.FIN);
         	this.mat.put("4",c5);
+        	this.mat.put("5",c5);
         	
-        	//c6
-        	HashMap<Etat, Etat> c6 = new HashMap<>();
-        	c6.put(Etat.E0, Etat.ERREUR);
-        	c6.put(Etat.H1, Etat.H);
-        	c6.put(Etat.H2, Etat.ERREUR);
-        	c6.put(Etat.H, Etat.ERREUR);
-        	c6.put(Etat.M1, Etat.M2);
-        	c6.put(Etat.M2, Etat.FIN);
-        	this.mat.put("5",c6);
-        	
-        	//c7
+        	//c7 c8 c9 c10
         	HashMap<Etat, Etat> c7 = new HashMap<>();
         	c7.put(Etat.E0, Etat.ERREUR);
         	c7.put(Etat.H1, Etat.H);
@@ -84,36 +66,9 @@ public enum Matrice {
         	c7.put(Etat.M1, Etat.ERREUR);
         	c7.put(Etat.M2, Etat.FIN);
         	this.mat.put("6",c7);
-        	
-        	//c8
-        	HashMap<Etat, Etat> c8 = new HashMap<>();
-        	c8.put(Etat.E0, Etat.ERREUR);
-        	c8.put(Etat.H1, Etat.H);
-        	c8.put(Etat.H2, Etat.ERREUR);
-        	c8.put(Etat.H, Etat.ERREUR);
-        	c8.put(Etat.M1, Etat.ERREUR);
-        	c8.put(Etat.M2, Etat.FIN);
-        	this.mat.put("7",c8);
-        	
-        	//c9
-        	HashMap<Etat, Etat> c9 = new HashMap<>();
-        	c9.put(Etat.E0, Etat.ERREUR);
-        	c9.put(Etat.H1, Etat.H);
-        	c9.put(Etat.H2, Etat.ERREUR);
-        	c9.put(Etat.H, Etat.ERREUR);
-        	c9.put(Etat.M1, Etat.ERREUR);
-        	c9.put(Etat.M2, Etat.FIN);
-        	this.mat.put("8",c9);
-        	
-        	//c10
-        	HashMap<Etat, Etat> c10 = new HashMap<>();
-        	c10.put(Etat.E0, Etat.ERREUR);
-        	c10.put(Etat.H1, Etat.H);
-        	c10.put(Etat.H2, Etat.ERREUR);
-        	c10.put(Etat.H, Etat.ERREUR);
-        	c10.put(Etat.M1, Etat.ERREUR);
-        	c10.put(Etat.M2, Etat.FIN);
-        	this.mat.put("9",c10);
+        	this.mat.put("7",c7);
+        	this.mat.put("8",c7);
+        	this.mat.put("9",c7);
         	
         	//c11
         	HashMap<Etat, Etat> c11 = new HashMap<>();
@@ -127,26 +82,82 @@ public enum Matrice {
         }
 
     },
-    M2("exo2",new HashMap<String, HashMap<Etat, Etat>>()){
+    M2("exo2",Etat.E0,new HashMap<String, HashMap<Etat, Etat>>()){
 
 		@Override
 		public void creeat() {
+        	
+            //C1
+        	HashMap<Etat, Etat> c1 = new HashMap<>();
+        	c1.put(Etat.E0, Etat.E1);
+        	c1.put(Etat.E1, Etat.ERREUR);
+        	c1.put(Etat.E2, Etat.ERREUR);
+        	c1.put(Etat.E3, Etat.ERREUR);
+        	this.mat.put(":",c1);
+        	
+            //c2
+        	HashMap<Etat, Etat> c2 = new HashMap<>();
+        	c2.put(Etat.E0, Etat.E2);
+        	c2.put(Etat.E1, Etat.ERREUR);
+        	c2.put(Etat.E2, Etat.ERREUR);
+        	c2.put(Etat.E3, Etat.ERREUR);
+        	this.mat.put(";",c2);
+        	
+            //c3
+        	HashMap<Etat, Etat> c3 = new HashMap<>();
+        	c3.put(Etat.E0, Etat.ERREUR);
+        	c3.put(Etat.E1, Etat.E3);
+        	c3.put(Etat.E2, Etat.E3);
+        	c3.put(Etat.E3, Etat.ERREUR);
+        	this.mat.put("-",c3);
+        	
+            //C4
+        	HashMap<Etat, Etat> C4 = new HashMap<>();
+        	C4.put(Etat.E0, Etat.ERREUR);
+        	C4.put(Etat.E1, Etat.E3);
+        	C4.put(Etat.E2, Etat.ERREUR);
+        	C4.put(Etat.E3, Etat.ERREUR);
+        	this.mat.put("-",C4);
+        	
+            //c5 c6
+        	HashMap<Etat, Etat> c5 = new HashMap<>();
+        	c5.put(Etat.E0, Etat.ERREUR);
+        	c5.put(Etat.E1, Etat.ERREUR);
+        	c5.put(Etat.E2, Etat.ERREUR);
+        	c5.put(Etat.E3, Etat.FIN);
+        	this.mat.put("(",c5);
+        	this.mat.put(")", c5);
+			
+		}
+    	
+    },
+    M3("exo3",Etat.E0,new HashMap<String, HashMap<Etat, Etat>>()){
+
+		@Override
+		public void creeat() {
+			// TODO Auto-generated method stub
 			
 		}
     	
     }
     ;
 
-    protected final String nom;
+    private final String nom;
+    private final Etat etat;
     protected HashMap<String, HashMap<Etat, Etat>> mat;
 
-    Matrice(String nom, HashMap<String, HashMap<Etat, Etat>> mat){
+    Matrice(String nom,Etat etat, HashMap<String, HashMap<Etat, Etat>> mat){
         this.nom=nom;
+        this.etat=etat;
         this.mat=mat;
         this.creeat();
     }
 
     public abstract void creeat();
+    
+    public final Etat getFirstEtat(){
+    	return this.etat;
+    }
 
     public final Etat get(String x, Etat y){
     	if(this.mat.containsKey(x)){
