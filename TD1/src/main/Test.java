3a28aa37c9c350ed40b4efaa3f691a53b3246e84
paul <paul604@users.noreply.github.com>
2016-11-16 11:21:01 +0100

    Update Test.java

--- TD1/src/main/Test.java
+++ TD1/src/main/Test.java
@@ -1,7 +1,5 @@
 package main;
 
-import java.awt.Color;
-
 import matrice.Etat;
 import matrice.Matrice;
 
@@ -12,8 +10,8 @@ import matrice.Matrice;
  */
 public class Test {
 
-	public static void test(Matrice m, Etat init, String text){
-		Etat etatCourant = init;
+	public static void test(Matrice m, String text){
+		Etat etatCourant = m.getFirstEtat();
 		int i =0;
 		while (etatCourant != Etat.FIN && etatCourant!= Etat.ERREUR) {
 			if (i>=text.length()) {break;}
