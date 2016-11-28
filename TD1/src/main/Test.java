4da95ec56f885e975c62bc7b76d76825619fe605
paul <paul604@users.noreply.github.com>
2016-11-28 17:28:37 +0100

    Update Test.java

--- TD1/src/main/Test.java
+++ TD1/src/main/Test.java
@@ -1,7 +1,9 @@
 package main;
 
 import matrice.Etat;
+import matrice.EtatC;
 import matrice.Matrice;
+import matrice.MatriceC;
 
 /**
  * Created by paul on 15/11/2016.
@@ -10,13 +12,13 @@ import matrice.Matrice;
  */
 public class Test {
 
+	//pour les enum
 	public static void test(Matrice m, String text){
 		Etat etatCourant = m.getFirstEtat();
 		int i =0;
 		while (etatCourant != Etat.FIN && etatCourant!= Etat.ERREUR) {
 			if (i>=text.length()) {
 				String c = "";
-
 				Etat etatNext = m.get(c+"", etatCourant);
 				System.out.println(etatCourant+" "+c+" "+etatNext);
 				etatCourant = etatNext;
@@ -31,6 +33,34 @@ public class Test {
 			}
 		}
 		if(etatCourant != Etat.FIN || i<text.length()){
+			System.out.println(text+" erreur");
+		}else{
+			System.out.println(text+" ok");
+		}
+	}
+
+	//pour les class
+	public static void test(MatriceC m, String text){
+		EtatC etatCourant = m.getEtatDep();
+		int i =0;
+		while (!etatCourant.getName().equalsIgnoreCase("fin")
+				&& !etatCourant.getName().equalsIgnoreCase("erreur") ) {
+			if (i>=text.length()) {
+				String c = "";
+				EtatC etatNext = m.get(c+"", etatCourant);
+				System.out.println(etatCourant+" "+c+" "+etatNext);
+				etatCourant = etatNext;
+				i++;
+			}else{
+				char c = text.charAt(i);
+
+				EtatC etatNext = m.get(c+"", etatCourant);
+				System.out.println(etatCourant+" "+c+" "+etatNext);
+				etatCourant = etatNext;
+				i++;
+			}
+		}
+		if(!etatCourant.getName().equalsIgnoreCase("fin") || i<text.length()){
 			System.out.println(text+" erreur");
 		}else{
 			System.out.println(text+" ok");
