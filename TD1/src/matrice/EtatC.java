395bd8cd54c4f66be5bd42e1ee6a83da965d3310
paul <paul604@users.noreply.github.com>
2016-11-28 17:20:53 +0100

    Create EtatC.java

---
+++ TD1/src/matrice/EtatC.java
@@ -0,0 +1,17 @@
+package matrice;
+
+/**
+ * Created by paul on 28/11/16.
+ */
+public class EtatC {
+
+    private String name;
+
+    public EtatC(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
